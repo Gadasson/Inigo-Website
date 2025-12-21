1c3d4054bcd542cae5301532c73d241a15aba0c4
Asaf Gadassi <asafgadassi@gmail.com>
2025-12-21 12:13:45 +0200

    Swap section order: move 'Create your place of calm' before World State section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <WorldState />
       <DiscoverReflect />
+      <WorldState />
       <Community />
       <EarlyAdopters />
       <FAQ />
